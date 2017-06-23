8de9966d505e92797405160e252201a7147e5474
A1daris <sizov.boris@gmail.com>
2017-06-23 09:52:38 +0300

    jQuery slider works

--- carousel jQuery/carousel_1.js
+++ carousel jQuery/carousel_1.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	function Slider(ID) {
+	function Slider(ID, loop) {
 		
 		this.slider = $("#" + ID);
 		this.buttonLeft = this.slider.find(".button-left");
@@ -9,32 +9,49 @@ $(document).ready(function() {
 		this.active = this.slides.filter(".active");
 		this.next = this.active.next();
 		this.prev = this.active.prev();
+		this.first = this.slides.first();
+		this.last = this.slides.last();
 
 		this.position = 0;
-				// console.log(this.next)
-		
+		var that = $(this);
+		this.buttonRight.on('click', this.moveRight.bind(this));
+		this.buttonLeft.on('click', this.moveLeft.bind(this));
+				// console.log( this.first)
+		}		
+
+		Slider.prototype.test = function() {
+			console.log(this.findActive);
+		}
+
 		Slider.prototype.findActive = function() {
 			this.active = this.slides.filter(".active");
 			this.next = this.active.next();
 			this.prev = this.active.prev();
 		}
-		
+
 		Slider.prototype.moveRight = function() {
 			this.findActive();
-			this.position -= (this.next.offset().left - this.active.offset().left);
-			this.setPosition();
-			this.next.addClass("active").siblings().removeClass("active");
+			if ( this.next.length) {
+				this.position -= (this.next.offset().left - this.active.offset().left);
+				this.setPosition();
+				this.next.addClass("active").siblings().removeClass("active");
+			}
 		}
 		Slider.prototype.moveLeft = function() {
-			this.findActive();
-			this.position += (this.active.offset().left - this.prev.offset().left)
-			this.setPosition();
-			this.prev.addClass("active").siblings().removeClass("active");
-		}
 
+			this.findActive();
+			if ( this.prev.length ) {
+				this.position += (this.active.offset().left - this.prev.offset().left)
+				this.setPosition();
+				this.prev.addClass("active").siblings().removeClass("active");
+			}
+
+		}
+		
 		Slider.prototype.setPosition = function() {
 			this.slideList.css('margin-left', this.position + 'px')
 		}
-	}
+	
 var c1 = new Slider('carousel-1');
 
+});
